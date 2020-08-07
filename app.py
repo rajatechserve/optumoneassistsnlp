7fa10b99afc904e8bf2f55f658294d254d8fc4fd
Raja <rajalingam.subramaniam@gmail.com>
2020-08-07 10:35:05 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Creating pickle file logic
 #   import pickle
+#   import pandas as pd 
 #   df= pd.read_csv("spam.csv", encoding="latin-1")
 # 	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
 # 	# Features and Labels
@@ -27,6 +28,8 @@
 
 import pickle
 from flask import Flask,render_template,url_for,request
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
 
 # load the model from disk
 filename = 'nlp_model.pkl'
@@ -39,15 +42,13 @@ def home():
 	return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
-def predict():   
-
-
-if request.method == 'POST':
-		message = request.form['message']
-		data = [message]
-		vect = cv.transform(data).toarray()
-		my_prediction = clf.predict(vect)
-	return render_template('result.html',prediction = my_prediction)
+def predict():       
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+    return render_template('result.html',prediction = my_prediction)
 
 if __name__ == '__main__':
 	app.run(debug=True)
